343ea7ed1b6fda5831072d424a2f93efc5df0c78
Jbar91 <jibarlucea91@gmail.com>
2023-06-22 21:08:16 +0200

    Add category field in Hero interface & values in Heroes mock

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Dr. Nice', category: 'Mutant' },
+  { id: 13, name: 'Bombasto', category: 'Mutant' },
+  { id: 14, name: 'Celeritas', category: 'Flyer' },
+  { id: 15, name: 'Magneta', category: 'Psychic' },
+  { id: 16, name: 'RubberMan', category: 'Mutant' },
+  { id: 17, name: 'Dynama', category: 'Flyer' },
+  { id: 18, name: 'Dr. IQ', category: 'Psychic' },
+  { id: 19, name: 'Magma', category: 'Mutant' },
+  { id: 20, name: 'Tornado', category: 'Mutant' },
 ];
