76040317adc44ebc5de3a388508fafad41885fbb
Jbar91 <jibarlucea91@gmail.com>
2023-06-22 21:32:53 +0200

    Update heroes variable in createDb with heroes mock

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,23 +1,14 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const heroes = [
-      { id: 12, name: 'Dr. Nice', category: 'Mutant' },
-      { id: 13, name: 'Bombasto', category: 'Mutant' },
-      { id: 14, name: 'Celeritas', category: 'Flyer' },
-      { id: 15, name: 'Magneta', category: 'Psychic' },
-      { id: 16, name: 'RubberMan', category: 'Mutant' },
-      { id: 17, name: 'Dynama', category: 'Flyer' },
-      { id: 18, name: 'Dr. IQ', category: 'Psychic' },
-      { id: 19, name: 'Magma', category: 'Mutant' },
-      { id: 20, name: 'Tornado', category: 'Mutant' },
-    ];
+    const heroes = HEROES;
     return { heroes };
   }
 
