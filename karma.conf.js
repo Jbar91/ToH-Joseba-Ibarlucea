e68789d8207f90f9dc81c61791ea095e0dbe25c7
Jbar91 <jibarlucea91@gmail.com>
2023-06-24 19:55:48 +0200

    Add angular.json tests configs

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "coverage/"),
       subdir: ".",
-      reporters: [{ type: "text" }, { type: "lcov" }],
+      reporters: [{ type: "lcov" }],
       includeAllSourcer: true,
     },
     reporters: ["mocha", "kjhtml"],
