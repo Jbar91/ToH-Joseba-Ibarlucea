c2b9f9312abd15571bed584dab568eb5ccdc2f14
Jbar91 <jibarlucea91@gmail.com>
2023-06-25 14:45:31 +0200

    Add categories service test cases

--- src/app/services/categories/categories.service.spec.ts
+++ src/app/services/categories/categories.service.spec.ts
@@ -1,19 +1,102 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CategoriesService } from './categories.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { MessageService } from '../message/message.service';
+import { HeroService } from '../hero/hero.service';
+import { Observable, of } from 'rxjs';
+import { mockHeroes, mockIdHeroes } from 'src/mocks/test.mocks';
+import { Hero } from 'src/app/hero';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
+  let messageService: MessageService;
+  let heroService: HeroService;
+  let http: HttpClientTestingModule;
+  let controller: HttpTestingController;
+
+  const mockHeroesService = {
+    getHeroes: jasmine.createSpy('getHeroes').and.returnValue(of(mockIdHeroes)),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HeroService,
+          useValue: mockHeroesService,
+        },
+      ],
     });
     service = TestBed.inject(CategoriesService);
+    messageService = TestBed.inject(MessageService);
+    heroService = TestBed.inject(HeroService);
+    http = TestBed.inject(HttpClientTestingModule);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should have an array of heroes', () => {
+    const heroes = service.heroes;
+
+    expect(heroes).toEqual([]);
+  });
+
+  it('Should have a BehaviorSubject of heroesInfo with initial values', () => {
+    let heroInfo: Hero[] = [];
+
+    service.heroesInfo$.subscribe((info) => {
+      heroInfo = info;
+    });
+    const initialInfo = [{ id: 0, name: '', category: '' }];
+
+    expect(heroInfo).toEqual(initialInfo);
+  });
+
+  describe('When the getHeroesInfo method is called', () => {
+    describe("And there's no errors", () => {
+      it("Should call the getHeroes method from the heroService and set the heroes property with it's response", () => {
+        service.getHeroesInfo().subscribe();
+
+        const firstHeroId = service.heroes[0].id;
+        const fisrtExpectedId = mockIdHeroes[0].id;
+
+        expect(firstHeroId).toEqual(fisrtExpectedId);
+      });
+
+      it('Should send a get request to the heroesUrl', () => {
+        service.getHeroesInfo().subscribe((heroes) => {
+          expect(heroes).toEqual(mockHeroes);
+        });
+
+        const req = controller.expectOne(service['heroesUrl']);
+        expect(req.request.method).toEqual('GET');
+        req.flush(mockHeroes);
+      });
+    });
+
+    describe("And there's an error", () => {
+      it('Should call the handleError method and log an error on console', () => {
+        service.getHeroesInfo().subscribe({
+          error: (err) => {
+            expect(err).toBeTruthy();
+          },
+        });
+        const error = spyOn(console, 'error');
+
+        const req = controller.expectOne(service['heroesUrl']);
+        expect(req.request.method).toEqual('GET');
+        req.error(new ProgressEvent('error'));
+
+        expect(error).toHaveBeenCalled();
+      });
+    });
   });
 });
--- src/mocks/test.mocks.ts
+++ src/mocks/test.mocks.ts
@@ -6,6 +6,14 @@ export const mockHeroes: Hero[] = [
   { name: 'test3', category: 'Psychic', id: 3 },
 ];
 
+export const mockIdHeroes = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+
+export const mockHeroesCategories = [
+  { name: 'test', category: 'Mutant' },
+  { name: 'test2', category: 'Flyer' },
+  { name: 'test3', category: 'Psychic' },
+];
+
 export const mockDeleteHeroes: Hero[] = [
   { name: 'test2', category: 'Flyer', id: 2 },
   { name: 'test3', category: 'Psychic', id: 3 },
@@ -13,7 +21,7 @@ export const mockDeleteHeroes: Hero[] = [
 
 export const mockInitialHeroes: Hero[] = [];
 
-export const mockAddHero = { name: 'Pepe', category: 'Mutant' };
+export const mockAddHero = { name: 'Pepe', category: 'Mutant', id: 4 };
 
 export const mockNoNameHero = { name: '', category: 'Mutant' };
 
